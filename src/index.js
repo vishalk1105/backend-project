c9be076ac0bb07dd74c958fb0c85d93d0e024edd
vishal vilas kamble <vishalvkamble95@gmail.com>
2024-09-11 19:39:56 +0530

    Error, utils code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 //require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("Mongo Db connection failed", err));
